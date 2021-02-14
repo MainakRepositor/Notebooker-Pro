4b9b69287a9f274568321f9ebd2becce8babf991
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-02-14 15:10:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st 
 import numpy as np 
 import pandas as pd  
-
+from plotly import *
 import plotly.express as px  
 from sklearn import *
 from lazypredict.Supervised import LazyRegressor
