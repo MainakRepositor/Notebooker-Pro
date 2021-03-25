0d8c92bd14741a76d7189fbe868065ee29b51cfd
MAINAK CHAUDHURI <64016811+MainakRepositor@users.noreply.github.com>
2021-03-25 09:40:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -456,6 +456,31 @@ def main():
         
     elif option == 'Documentation':
         st.subheader("How to use Notebooker Pro")
+        st.markdown('''The notebooker pro is a user-friendly software designed to help you make a good data science notebook in few steps.
+Well, notebooker pro will not be making a notebook for you, but will provide you with all the data insights that you 
+will need to put in your kernel. The notebooker pro has been provided with 4 major sections:
+
+i.  EDA (Explanatory Data Analysis)  --> used to find important data and statistical insights from the uploaded files
+
+ii. Visualization --> Used to perform data visualization with 5 basic important types of graphs
+
+iii.Regression --> Loops through 30 different regression models and returns the complexity statistics of the result
+		   of regression modelling for your dataset for chosen seed values and size. The only thing to keep in
+		   mind while using this is that, the data must be fitting with a regression modelling. Datasets used
+		   for classification algorithm might generate vague results. So use a proper dataset.
+		   **[eg.: do not use iris,cancer,penguins etc. classifier dataset]**
+
+iv. Classification --> Loops through 30 different classification models and returns the complexity statistics of the result
+		   of classification modelling for your dataset for chosen seed values and size. The only thing to keep in
+		   mind while using this is that, the data must be fitting with a classification modelling. Datasets used
+		   for non-classification algorithm might generate vague results. So use a proper dataset.
+		   
+
+Features:
+Upload file => Upload only csv files.
+Data split  => This is a linear slidebar, that will let you choose split ratio between 0 to 1
+Random seed => Helps to randomize the data in training and testing data samples. 
+	       You may change to get the best accuracy of for a particular model.''')
         
     elif option == 'About Us':
         st.subheader("About Us 😊")
